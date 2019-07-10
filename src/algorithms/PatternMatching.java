fc2121f67ee9ce18348573e57664b42466b47fff
ManishaRana1195 <manisha1195@gmail.com>
2019-07-10 13:34:25 -0500

    pattern matching for overlapping pattern

--- src/algorithms/PatternMatching.java
+++ src/algorithms/PatternMatching.java
@@ -8,12 +8,30 @@ public class PatternMatching {
     String inputString = "AABAACAADAABAABA";
 
     matchPattern(pattern, inputString);
+    System.out.println("*******************");
+    matchOverlappingPattern(pattern, inputString);
+  }
+
+  private static void matchOverlappingPattern(String pattern, String input) {
+    int patternLen = pattern.length();
+    int inputLen = input.length();
+    for (int i = 0; i <= inputLen - patternLen; i++) {
+      int j;
+      for (j = 0; j < patternLen; j++) {
+        if (pattern.charAt(j) != input.charAt(i+j)) {
+          break;
+        }
+      }
+      if (j == patternLen) {
+        System.out.println("Pattern found at " + (i));
+      }
+    }
   }
 
   private static void matchPattern(String pattern, String input) {
     int patternLen = pattern.length();
     int inputLen = input.length();
-    for (int i = 0; i < inputLen - patternLen; i++) {
+    for (int i = 0; i <= inputLen - patternLen; i++) {
       for (int j = 0; j < patternLen; ) {
         if (pattern.charAt(j) != input.charAt(i)) {
           break;
@@ -28,5 +46,7 @@ public class PatternMatching {
         }
       }
     }
+
+
   }
 }
