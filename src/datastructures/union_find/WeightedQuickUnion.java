65d668a5d1385f13fbf60e59428622f8b0f8bfc0
ManishaRana1195 <manisha1195@gmail.com>
2019-07-10 16:23:27 -0500

    union-find in complexity log n

---
+++ src/datastructures/union_find/WeightedQuickUnion.java
@@ -0,0 +1,48 @@
+package datastructures.union_find;
+
+public class WeightedQuickUnion {
+  private int _numberOfNodes;
+  private int[] _nodes;
+  private int[] _treeSize;
+
+  public WeightedQuickUnion(int numberOfNodes) {
+    _numberOfNodes = numberOfNodes;
+    _nodes = new int[numberOfNodes];
+    for (int i = 0; i < numberOfNodes; i++) {
+      _nodes[i] = i;
+      _treeSize[i] = 1;
+    }
+  }
+
+  /*union method that connects the tree based on the size of the tree.
+   * Here, a small tree is linked to the tall tree, that results into tree
+   * with less depth and more width*/
+  private void union(int nodeP, int nodeQ) {
+    int rootP = root(nodeP);
+    int rootQ = root(nodeQ);
+
+    if (rootP == rootQ) return;
+
+    int sizeTreeP = _treeSize[rootP]; /* size should be calculated for the root*/
+    int sizeTreeQ = _treeSize[rootQ];
+
+    if (sizeTreeP > sizeTreeQ) {
+      _nodes[rootQ] = rootP;
+      _treeSize[rootQ] += _treeSize[rootP];
+    } else {
+      _nodes[rootP] = rootQ;
+      _treeSize[rootP] += _treeSize[rootQ];
+    }
+  }
+
+  private boolean connected(int nodeP, int nodeQ) {
+    return root(nodeP) == root(nodeQ);
+  }
+
+  private int root(int nodeP) {
+    while (nodeP != _nodes[nodeP])
+      nodeP = _nodes[nodeP];
+
+    return nodeP;
+  }
+}
