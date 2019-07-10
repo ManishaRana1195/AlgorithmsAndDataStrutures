daf90d68d2f720b7125faa1ab8466b3640c52b52
ManishaRana1195 <manisha1195@gmail.com>
2019-07-10 14:24:42 -0500

    union-find in complexity n

--- src/datastructures/union_find/UnionFind.java
+++ src/datastructures/union_find/UnionFind.java
@@ -1,6 +1,6 @@
 package datastructures.union_find;
 
-public class UnionFind {
+class UnionFind {
 
   private int _numberOfNodes;
   private Integer[] _nodes;
@@ -13,16 +13,12 @@ public class UnionFind {
     }
   }
 
+  /* a method that take two nodes and merge their components
+  if the nodes are not connected. If nodeP and nodeQ are not
+  connected, it takes the id of nodeP and updates where nodeQ
+  id is stored*/
   void union(int nodeP, int nodeQ) {
-    if (nodeP < 0 || _numberOfNodes < nodeP) {
-      System.out.println("Node not within range");
-      return;
-    }
-
-    if (nodeQ < 0 || _numberOfNodes < nodeQ) {
-      System.out.println("Node not within range");
-      return;
-    }
+    if (checkNodeWithinRange(nodeP, nodeQ)) return;
 
     if (!connected(nodeP, nodeQ)) {
       Integer nodeQComponentId = _nodes[nodeQ];
@@ -35,9 +31,22 @@ public class UnionFind {
 
   }
 
+  private boolean checkNodeWithinRange(int nodeP, int nodeQ) {
+    if (nodeP < 0 || _numberOfNodes < nodeP) {
+      System.out.println("Node not within range");
+      return true;
+    }
+
+    if (nodeQ < 0 || _numberOfNodes < nodeQ) {
+      System.out.println("Node not within range");
+      return true;
+    }
+    return false;
+  }
+
 
   boolean connected(int nodeP, int nodeQ) {
     return _nodes[nodeP].equals(_nodes[nodeQ]);
   }
-  
+
 }
