70505ec994c4b0ad013a8d55b597ae3013471abb
ManishaRana1195 <manisha1195@gmail.com>
2019-09-03 23:52:01 -0500

    added BST add and delete operation

--- src/algorithms/LinkedListIntersection.java
+++ src/algorithms/LinkedListIntersection.java
@@ -4,43 +4,13 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class LinkedListIntersection {
-
   public static void main(String[] args) {
-    String a = "pale";
-    String b = "spale";
+    LinkedList<Object> list1 = new LinkedList<>(List.of(3, 4, 7, 6, 8, 10));
+    LinkedList<Object> list2 = new LinkedList<>(List.of(2, 5, 8, 10));
 
-    System.out.println("Are the strings, " + a + " and " + b + " one edit away: " + isOneEditAway(a, b));
-    a = "pale";
-    b = "pabe";
-    System.out.println("Are the strings, " + a + " and " + b + " one edit away: " + isOneEditAway(a, b));
+
   }
 
-  private static boolean isOneEditAway(String a, String b) {
-    List<String> stringA = new LinkedList<>(List.of(a.split("")));
-    List<String> stringB = new LinkedList<>(List.of(b.split("")));
 
-    int sizeStringB = stringB.size();
-    int sizeStringA = stringA.size();
-
-    if (Math.abs(sizeStringA - sizeStringB) > 1) {
-      return false;
-    }
-
-    int i = 0, j = 0, count = 0;
-    while (i != sizeStringA || j != sizeStringB) {
-      if (stringA.get(i).equals(stringB.get(j))) {
-        i++;
-        j++;
-      } else {
-        count += 1;
-        if (sizeStringA > sizeStringB) {
-          i++;
-        } else {
-          j++;
-        }
-      }
-    }
-
-    return count <= 1;
-  }
 }
+
--- src/datastructures/trees/BinarySearchTree.java
+++ src/datastructures/trees/BinarySearchTree.java
@@ -1,44 +1,152 @@
 package datastructures.trees;
 
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 /*
  * Tree is undirected graph, with no cycles
  * each node to connected to another only through one path
  * with N nodes, connected with n-1 edges
  * */
 public class BinarySearchTree {
-  private Node root;
+
+  private Node root = null;
 
   public void insertNode(Integer value) {
     Node newNode = new Node(value, null, null);
-    if (root == null) {
-      root = newNode;
-      return;
-    }
-    Node node = root;
-    while (node != null) {
-      if (value <= node.getValue()) {
-        node = node.getLeftNode();
-      }
-      if (value > node.getValue()) {
-        node = node.getRightNode();
-      }
-
-    }
-    node.setLeftNode(newNode);
-    node.setRightNode(newNode);
+    root = insert(root, newNode);
   }
 
-  public void deleteNode(Node node) {
+  private Node insert(Node node, Node newNode) {
+    if (node == null) {
+      node = newNode;
+    } else if (newNode.getValue() <= node.getValue()) {
+      node.setLeftNode(insert(node.getLeftNode(), newNode));
+    } else {
+      node.setRightNode(insert(node.getRightNode(), newNode));
+    }
 
+    return node;
+  }
+
+  public void deleteNode(int searchNode) {
+    delete(root, searchNode);
+  }
+
+  /*1. Find the element that needs to be deleted
+   * 2. Replace that element with the appropriate child element*/
+  private Node delete(Node node, Integer searchNode) {
+    if (root == null) {
+      return null;
+    }
+
+    if (searchNode < node.getValue()) {
+      node.setLeftNode(delete(node.getLeftNode(), searchNode));
+    } else if (searchNode > node.getValue()) {
+      node.setRightNode(delete(node.getRightNode(), searchNode));
+    } else {
+      /* you have reached the point to delete the node. We have three cases to delete a node.
+        1. if the node is leaf node
+        2. if the node has either left of right child
+        3. if the node has both the child nodes
+        4. if the node doesnot exist in the tree
+
+*/
+      if (node.getLeftNode() == null) {
+        Node rightChild = node.getRightNode();
+        node.setValue(null);
+        node = null;
+        return rightChild;
+      } else if (node.getRightNode() == null) {
+        Node leftChild = node.getLeftNode();
+        node.setValue(null);
+        node = null;
+        return leftChild;
+      } else {
+        Node smallestRightNode = digLeft(node.getRightNode());
+        node.setValue(smallestRightNode.getValue());
+        node.setRightNode(delete(node.getRightNode(), smallestRightNode.getValue()));
+      }
+    }
+    return node;
+  }
+
+  private Node digLeft(Node rightNode) {
+    Node node = rightNode;
+    while (node.getLeftNode() != null) {
+      node = node.getLeftNode();
+    }
+    return node;
+  }
+
+
+  /*to get prefix expression on of an expression tree*/
+  public void preorder(Node node) {
+    if (node == null) return;
+    System.out.println(node.getValue());
+    preorder(node.getLeftNode());
+    preorder(node.getRightNode());
+  }
+
+  /* postfix expression of an expression tree */
+  public void postorder(Node node) {
+    if (node == null) return;
+
+    postorder(node.getLeftNode());
+    postorder(node.getRightNode());
+    System.out.println(node.getValue());
+  }
+
+  /* to get nodes of tree in sorted order*/
+  public void inorder(Node node) {
+    if (node == null) return;
+    inorder(node.getLeftNode());
+    System.out.println(node.getValue());
+    inorder(node.getRightNode());
+  }
+
+  public void levelOrder() {
+    LinkedList<Node> visitedNodes = new LinkedList<>();
+    HashMap<Integer, Boolean> isVisited = new HashMap<>();
+    visitedNodes.push(root);
+    breadthFirstSearch(isVisited, visitedNodes);
+  }
+
+  private void breadthFirstSearch(HashMap<Integer, Boolean> isVisited, LinkedList<Node> visitedNodes) {
+    while (!visitedNodes.isEmpty()) {
+      Node currentNode = visitedNodes.remove();
+      if (currentNode != null && !isVisited.getOrDefault(currentNode.getValue(), false)) {
+        System.out.println(" " + currentNode.getValue());
+        isVisited.put(currentNode.getValue(), true);
+        visitedNodes.push(currentNode.getLeftNode());
+        visitedNodes.push(currentNode.getRightNode());
+        breadthFirstSearch(isVisited, visitedNodes);
+      }
+    }
   }
 
   public void print() {
-
+    System.out.println("Nodes in the BST:");
+    inorder(this.root);
   }
 
-  public static void main(String[] args) {
 
+  public static void main(String[] args) {
+    BinarySearchTree tree = new BinarySearchTree();
+    tree.insertNode(50);
+    tree.insertNode(80);
+    tree.insertNode(40);
+    tree.insertNode(70);
+    tree.insertNode(10);
+    tree.insertNode(15);
+    tree.insertNode(230);
+    tree.print();
+    System.out.println("***************************************************************");
+    tree.deleteNode(40);
+    tree.deleteNode(80);
+    tree.deleteNode(50);
+    tree.levelOrder();
   }
 }
 
