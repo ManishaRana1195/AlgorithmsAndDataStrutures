9fb995b4eb737ab8a392357593064da59c01db7d
ManishaRana1195 <manisha1195@gmail.com>
2019-11-22 14:59:39 -0600

    longest palindrome substring

---
+++ src/algorithms/LongestPalindromeSubstring.java
@@ -0,0 +1,39 @@
+package algorithms;
+
+public class LongestPalindromeSubstring {
+    public static String longestPalindrome(String s) {
+        int len = s.length();
+        int max = 0;
+        String maxSub = "";
+        if (len == 1) return s;
+        if (len == 2) {
+            if (isPalindrome(0, 1, s)) return s;
+        }
+        for (int i = 0; i < len; i++) {
+            for (int j = i; j <= len; j++) {
+                if (isPalindrome(i, j - 1, s)) {
+                    int length = j - i;
+                    if (max < length) {
+                        maxSub = s.substring(i, j);
+                        max = length;
+                    }
+                }
+            }
+        }
+
+        return maxSub;
+    }
+
+    private static boolean isPalindrome(int i, int j, String sub) {
+        if (i >= j) return true;
+
+        String charI = String.valueOf(sub.charAt(i));
+        String charJ = String.valueOf(sub.charAt(j));
+        return charI.equals(charJ) && isPalindrome(i + 1, j - 1, sub);
+    }
+
+    public static void main(String[] args) {
+        String input = "ababccddddd";
+        System.out.println(longestPalindrome(input));
+    }
+}
